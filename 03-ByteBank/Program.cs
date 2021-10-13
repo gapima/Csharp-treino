94806ac85ad8d128461fb0fd5c4a3a4c416ff958
gapima <71856172+gapima@users.noreply.github.com>
2021-10-13 07:50:59 -0300

    ByteBank
    Treinando chsharp

---
+++ 03-ByteBank/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 86452;
+            contaDaGabriela.saldo = 100;
+
+            ContaCorrente contaDaGabrielaCosta = new ContaCorrente();
+            contaDaGabrielaCosta.titular = "Gabriela";
+            contaDaGabrielaCosta.agencia = 863;
+            contaDaGabrielaCosta.numero = 86452;
+            contaDaGabriela.saldo = 100;
+             
+            //Console.WriteLine(contaDaGabriela == contaDaGabrielaCosta);
+
+
+            if(contaDaGabriela.saldo >= 100)
+            {
+                contaDaGabriela.saldo -= 100;
+            }
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
