326e8f4cb5875e66f8d14e239dbbe11cdb3d1be2
gapima <71856172+gapima@users.noreply.github.com>
2021-10-13 22:24:20 -0300

    Construtores e membros staticos

---
+++ 07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,109 @@
+﻿//using _06_ByteBank;
+
+
+namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+
+        public Cliente Titular { get; set; }
+        public static int TotalDeContasCriadas { get; private set; }
+
+
+        private int _agencia;
+        public int Agencia {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    return;
+                }
+                _agencia = value;
+            }
+        }
+        public int Numero { get; set; }
+        
+        private double saldo = 100;
+        public double Saldo
+        {
+            get
+            {
+                return saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                saldo = value;
+            }
+        }
+
+        //public void SetSaldo(double saldo)
+        //{
+        //    if(saldo < 0)
+        //    {
+        //        return;
+        //    }
+          
+        //    this.saldo = saldo;
+        //}
+        //public double GetSaldo()
+        //{
+        //    return saldo;
+        //}
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalDeContasCriadas++;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+
+            saldo -= valor;
+            return true;
+
+
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+
+            saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+
+        }
+
+
+
+
+
+
+    }
+
+}
---
+++ 07-ByteBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            ContaCorrente conta1 = new ContaCorrente(0000, 55555);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            ContaCorrente conta2 = new ContaCorrente(555, 00000);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            ContaCorrente conta3 = new ContaCorrente(111, 50000);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+        }
+    }
+}
